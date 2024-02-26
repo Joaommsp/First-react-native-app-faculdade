74864d5970c6b5e5c96313af0d34f93ef0f75ba2
João Marcos Melo Santos Pinto <212.14.017@EDU.gruposete.local>
2024-02-26 19:58:02 -0300

    final da aula 26/02

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image  } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
@@ -8,9 +8,17 @@ export default function App() {
       <Text> CR7 </Text>
       <StatusBar style="auto" />
       <Image
-    style={{width:  150, height:  150}}
-    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX-b_T6RqK6jKHLTpg-8Fbi9cL2qSUJYttS75woXRLYw&s'}}
-  />
+        style={{ width: 150, height: 150 }}
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX-b_T6RqK6jKHLTpg-8Fbi9cL2qSUJYttS75woXRLYw&s",
+        }}
+      />
+      <Image
+        style={{ width: 150, height: 150 }}
+        source={{
+          uri: "https://ih1.redbubble.net/image.5160944362.1058/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg",
+        }}
+      />
     </View>
   );
 }
@@ -18,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
