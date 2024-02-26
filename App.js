74b914b5138fd6d375995b42934621a11ce0ecb3
João Marcos Melo Santos Pinto <212.14.017@EDU.gruposete.local>
2024-02-26 19:50:31 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>CR7 CARECAKK</Text>
+      <Text> CR7 </Text>
       <StatusBar style="auto" />
+      <Image
+    style={{width:  150, height:  150}}
+    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX-b_T6RqK6jKHLTpg-8Fbi9cL2qSUJYttS75woXRLYw&s'}}
+  />
     </View>
   );
 }
